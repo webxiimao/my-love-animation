7c58bb3292ef9741082a61967a8463b372afd8d4
webxiimao <web_xiimao@outlook.com>
2019-10-08 15:49:07 +0800

    增加贝塞尔曲线绘制的模型

--- study/chapter5-贝塞尔曲线爱心/index.js
+++ study/chapter5-贝塞尔曲线爱心/index.js
@@ -21,18 +21,27 @@ const Config = {
 function BezierLine(){
   this.time = Config.time * 1000;
   this.size = Config.size
+  this.start = [canvas.width / 2, canvas.height / 2]
+  this.cp = [200, 100]
+  this.end = [100, 200]
 }
 
+
+
 BezierLine.prototype.draw = function(timestamp){
-  console.log(timestamp);
   
   if(timestamp >= this.time)return false
+  let current = timestamp / this.time;
+  let v1 = [this.cp[0] - this.start[0], this.cp[1] - this.start[1]]
+  let pv1 = [v1[0] * current, v1[1] * current];
+  let p1 = [pv1[0] + this.start[0], pv1[1] + this.start[1]]
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.beginPath();
-  context.moveTo(canvas.width / 2, 0);
-  let pt = timestamp / this.time;
-  let currentSize = pt * this.size;
-  context.lineTo(canvas.width / 2, currentSize);
+  context.moveTo(this.start[0], this.start[1]);
+  let p2x = Math.pow((1 - current), 2) * this.start[0] + 2 * current * (1-current) * this.cp[0] + Math.pow(current, 2) * this.end[0]
+  let p2y = Math.pow((1 - current), 2) * this.start[1] + 2 * current * (1-current) * this.cp[1] + Math.pow(current, 2) * this.end[1]
+  let p2 = [p2x, p2y]
+  context.quadraticCurveTo(p1[0], p1[1], p2[0], p2[1]);
   context.stroke();
   
   return true
@@ -42,6 +51,7 @@ let bezierLine = new BezierLine()
 
 function drawLine(timestamp){
   if(bezierLine.draw(timestamp))window.requestAnimationFrame(drawLine)
+  // bezierLine.draw()
 }
 
 
