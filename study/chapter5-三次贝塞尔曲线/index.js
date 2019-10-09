bb457249f30305bfeba51c58603a2639de6bc757
webxiimao <web_xiimao@outlook.com>
2019-10-09 11:14:58 +0800

    三次贝塞尔曲线路径

--- study/chapter5-三次贝塞尔曲线/index.js
+++ study/chapter5-三次贝塞尔曲线/index.js
@@ -37,7 +37,13 @@ BezierLine.prototype.draw = function(timestamp){
   context.beginPath();
   context.moveTo(this.start[0], this.start[1]);
   let p1 = [this.start[0] + this.v1[0] * current, this.start[1] + this.v1[1] * current]
-  let p2 = [this.start[0] + this.v1[0] + this.v2[0] * current, this.start[1] + this.v1[1] + this.v2[1] * current]
+  // let p2 = [this.start[0] + this.v1[0] + this.v2[0] * current, this.start[1] + this.v1[1] + this.v2[1] * current]
+  let p2x = Math.pow((1 - current), 2) * this.start[0]
+      + 2 * current * (1 - current) * (this.start[0] + this.v1[0])
+      + Math.pow(current, 2) * (this.start[0] + this.v1[0] + this.v2[0])
+  let p2y = Math.pow((1 - current), 2) * this.start[1]
+      + 2 * current * (1 - current) * (this.start[1] + this.v1[1])
+      + Math.pow(current, 2) * (this.start[1] + this.v1[1] + this.v2[1])
   let p3x = Math.pow((1 - current), 3) * this.start[0] 
       + 3 * Math.pow((1-current), 2) * current * (this.start[0] + this.v1[0]) 
       + 3 * Math.pow(current, 2) * (1-current) * (this.start[0] + this.v1[0] + this.v2[0]) 
@@ -46,7 +52,10 @@ BezierLine.prototype.draw = function(timestamp){
       + 3 * Math.pow((1-current), 2) * current * (this.start[1] + this.v1[1]) 
       + 3 * Math.pow(current, 2) * (1-current) * (this.start[1] + this.v1[1] + this.v2[1]) 
       + this.end[1] * Math.pow(current, 3)
-  context.bezierCurveTo(p1[0], p1[1], p2[0], p2[1], p3x, p3y);
+  // if(timestamp <= 1000){
+  // }
+  
+  context.bezierCurveTo(p1[0], p1[1], p2x, p2y, p3x, p3y);
   
   context.stroke();
   
